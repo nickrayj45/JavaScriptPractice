f62cf66afd85bda83df58e35cd2d5f100abe657a
Nicholas Jaquez <nrjaquez@gmail.com>
2020-08-24 22:00:52 -0700

    color added, javascript tested

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// console.log("Hello")
+// var a = 2;
+// var b = 7;
+// console.log(a * b);
+
+var firstName = "Nick";
+var age =31;
+
+var drink = age >=21 ? "beer" : "juice";
+console.log(drink);
