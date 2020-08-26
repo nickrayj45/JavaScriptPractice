4b2b05f51ce6044d35ff43bb44cf5bea3c07eb45
Nicholas Jaquez <nrjaquez@gmail.com>
2020-08-25 20:58:45 -0700

    bootstrap added

--- scripts.js
+++ scripts.js
@@ -3,8 +3,24 @@
 // var b = 7;
 // console.log(a * b);
 
-var firstName = "Nick";
-var age =31;
+var firstName = "Nick ";
+var lastName = "Jaquez"
+var age = 31;
+var job = "Content Manager";
 
-var drink = age >=21 ? "beer" : "juice";
-console.log(drink);
+// var drink = age >=21 ? "beer" : "juice";
+// console.log(drink);
+
+// if (age >= 21) {
+//   var drink = "beer";
+// } else {
+//   var drink = "juice";
+// }
+// console.log(drink)
+
+console.log(firstName + lastName + " is " + age + " years old." + " He is a " + job + ".")
+
+for (var i=1; i < 10; i++) {
+    console.log(i)
+}
+
